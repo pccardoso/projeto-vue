6604815f2982249d48cecd734cfa73290cfacc02
Paulo Cesar <paulo.cardoso2408@gmail.com>
2025-04-07 16:07:37 -0300

    Alterações realizadas na minha última branch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Alterado para a Branch feature/teste2"/>
+  <HelloWorld msg="Estou nessa branch feature/teste3"/>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis minus cum totam veritatis commodi eius! Quasi aliquid earum sit, tempore harum obcaecati cupiditate quae, reprehenderit soluta, commodi recusandae facilis! </p>
 </template>
 
 <script>
